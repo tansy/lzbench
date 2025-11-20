5b33c02689352ec4f82d1344c41dfeef3f0f3ef9
tansy <tansy@wir.pl>
2025-11-20 14:07:04 +0000

    fix release download

--- build/upd-repos.sh
+++ build/upd-repos.sh
@@ -15,63 +15,97 @@ function SPLIT()
     STR="$2"
     IFS="$SEP" read -ra array <<< "$STR"
     for i in "${array[@]}"; do
-        : ;
+        :
     done
     }
 
+function check_dir_and_cont()
+    {
+    dir="$1"
+    if [ ! -d "$dir" ]; then
+        read -sp "(codec) directory \'$dir\' not found. Continue anyway? [y/N] " yes_no
+        if [ $yes_no == "y" ] || [ $yes_no == "Y" ] || [ $yes_no == "yes" ] || [ $yes_no == "YES" ]; then
+            :
+        else
+            exit 2
+        fi
+    fi
+    }
+
+KEEP=
+
+ADDR=
 REPO=
 TAG=
+ARCH=
 
-NULL=/dev/null # "nul" for cygwin/msys
+NULL=/dev/null # "nul" for cygwin/msys; /dev/stderr if you want see progress output
+NULL=/dev/stderr
 WGET=wget
 WGETSO="wget -O-"
 #WGETSO="curl -s"
 
-update_repo()
+update_tag()
     {
     repo_url="$1"
-    shift
+    ADDR="$repo_url"
     SPLIT '/' "$repo_url"
     asize=${#array[@]}
-    repo=${array[$(($asize-1))]}
-    owner=${array[$(($asize-2))]}
+    REPO=${array[$(($asize-1))]}
+    OWNER=${array[$(($asize-2))]}
 
-    latest_tag=$( $WGETSO 2>$NULL "https://api.github.com/repos/$owner/$repo/releases/latest" \
+    latest_tag=$( $WGETSO "https://api.github.com/repos/$OWNER/$REPO/releases/latest" 2>$NULL \
         | grep -Po '"tag_name": *"\K.*?(?=")' )
-    tag=${latest_tag##v}
-    if [ -z "$tag" ]; then echo "Error: no latest_tag found."; exit 2; fi
-    addr="${repo_url}/archive/refs/tags/${latest_tag}.zip"
-    arch="${latest_tag}.zip"
+    TAG=${latest_tag##v}
+    if [ -z "$TAG" ]; then echo "Error: latest_tag not found."; return 2; fi
+    ARCH="${repo_url}/archive/refs/tags/${latest_tag}.zip"
+    }
 
-    if [ ! -f "${repo}-${branch}.zip" ]; then
-        $WGET "${addr}" && \
-        mv $arch "${repo}-${tag}.zip"
+update_repo()
+    {
+    echo "update_repo()"
+    repo_url="$1"
+    shift
+
+    if [ -z $TAG ]; then
+        update_tag "$1" || echo "latest_tag not found" && exit 2
     fi
-    arch="${repo}-${tag}.zip"
+    arch="$REPO-$TAG.zip"
 
-    local dir="${repo}-${tag}"
+    if [ ! -f "$arch" ]; then
+        $WGETSO "${ARCH}" > $arch 2>$NULL
+    fi
+
+    local dir="${REPO}-${TAG}"
     [ -d $dir ] || unzip -q "$arch"
 
-    mkdir -p "$repo"
+    mkdir -p "$REPO"
 
-    REPO=$repo
-    TAG=$tag
     for i in "$@"; do # commands
         cmd="$1"
         eval "$cmd"
         shift
     done && \
-#        rm -Rf "$dir"
-#        rm -f "$arch"
-        :
+    if [ -z $KEEP ]; then
+        rm -Rf "$dir"
+        rm -f "$arch"
+    fi
+    :
     }
 
 usage()
     {
-    echo "update repos/codecs"
-    echo "upd-repo <repository>"
-    echo "example: cd lzbench/lz/ && sh ../build/upd-repos.sh zlib"
-    echo "for available codecs refer to source of main loop"
+    cat << EOF
+update bench repos/codecs
+upd-repo [option] <repository>
+    options:
+    --keep    keep temporary files/directories
+    --help    print help screen
+    for available codecs refer to source of main loop
+
+example:
+    cd lzbench/lz/ && bash ../build/upd-repos.sh zlib
+EOF
     }
 
 
@@ -87,33 +121,52 @@ while [[ $# -gt 0 ]]; do
     case $1 in
         # update_repo https://github.com/user/repo repo master-branch "cp repo-master/*.c repo"
     "7-zip")
-        update_repo https://github.com/ip7z/7zip 7zip main "cp 7zip-main/C/* 7-zip"
+        check_dir_and_cont 7-zip
+        update_tag https://github.com/ip7z/7zip
+        update_repo $ADDR "cp 7zip-$TAG/C/* 7-zip"
         ;;
     "bzip3")
-        update_repo https://github.com/iczelia/bzip3 bzip3 master "cp -r bzip3-master/src bzip3" "cp -r bzip3-master/include bzip3" "cp bzip3-master/README.md bzip3"
+        check_dir_and_cont bzip3
+        update_tag https://github.com/iczelia/bzip3
+        update_repo $ADDR "cp -r bzip3-$TAG/src bzip3" "cp -r bzip3-$TAG/include bzip3" "cp bzip3-$TAG/README.md bzip3"
         ;;
     "libdeflate")
-        update_repo https://github.com/ebiggers/libdeflate libdeflate master "cp libdeflate-master/* libdeflate" "cp -r libdeflate-master/lib libdeflate"
+        check_dir_and_cont libdeflate
+        update_tag https://github.com/ebiggers/libdeflate
+        update_repo $ADDR "cp libdeflate-$TAG/* libdeflate" "cp -r libdeflate-$TAG/lib libdeflate"
         ;;
     "lz4")
-        update_repo https://github.com/lz4/lz4 lz4 dev "cp -r lz4-dev/lib lz4" "rm -Rf lz4/lib/dll"
+        check_dir_and_cont lz4
+        update_tag https://github.com/lz4/lz4
+        update_repo $ADDR "cp -r lz4-$TAG/lib lz4" "rm -Rf lz4/lib/dll"
         ;;
     "snappy")
-        update_repo https://github.com/google/snappy snappy main "cp snappy-main/* snappy"
+        check_dir_and_cont snappy
+        update_tag https://github.com/google/snappy
+        update_repo $ADDR "cp snappy-$TAG/* snappy"
         ;;
     "xz")
-        update_repo https://github.com/tukaani-project/xz xz master "cp -r xz-master/src xz"
+        check_dir_and_cont xz
+        update_tag https://github.com/tukaani-project/xz
+        update_repo $ADDR "cp -r xz-$TAG/src xz"
         ;;
     "zlib")
-        echo "REPO=$REPO"
-        echo "TAG=$TAG"
-        update_repo https://github.com/madler/zlib "cp zlib-$TAG/* zlib"
+        check_dir_and_cont zlib
+        update_tag https://github.com/madler/zlib
+        update_repo $ADDR "cp zlib-$TAG/* zlib"
         ;;
     "zlib-ng")
-        update_repo https://github.com/zlib-ng/zlib-ng zlib-ng develop "cp zlib-ng-develop/*.c zlib-ng-develop/*.h zlib-ng" "cp -r zlib-ng-develop/arch zlib-ng" "cp -r zlib-ng-develop/README.md zlib-ng"
+        check_dir_and_cont zlib-ng
+        update_tag https://github.com/zlib-ng/zlib-ng
+        update_repo $ADDR "cp zlib-ng-$TAG/*.c zlib-ng-$TAG/*.h zlib-ng" "cp -r zlib-ng-$TAG/arch zlib-ng" "cp -r zlib-ng-$TAG/README.md zlib-ng"
         ;;
     "zstd")
-        update_repo https://github.com/facebook/zstd zstd dev "cp -r zstd-dev/lib zstd" "cp -r zstd-dev/README.md zstd" "cp -r zstd-dev/CHANGELOG zstd"
+        check_dir_and_cont zstd
+        update_tag https://github.com/facebook/zstd
+        update_repo $ADDR "cp -r zstd-$TAG/lib zstd" "cp -r zstd-$TAG/README.md zstd" "cp -r zstd-$TAG/CHANGELOG zstd"
+        ;;
+    --keep)
+        KEEP="1"
         ;;
     --all)
         $0 lz4 xz zlib zstd
